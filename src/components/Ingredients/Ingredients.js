729a11ca4bee06ce541306f360f7d08cacf62e81
Forest1027 <azureforestryan@gmail.com>
2021-01-29 08:48:18 +0000

    sending http requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,7 +8,15 @@ const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
 
     const addIngredientHandler = ingredient => {
-        setUserIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredient}]);
+        fetch('https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient),
+            headers: {'Content-Type': 'application/json'}
+        }).then(response => {
+            return response.json();
+        }).then(responseDate => {
+            setUserIngredients(prevIngredients => [...prevIngredients, { id: responseDate.name, ...ingredient}]);
+        });
     };
 
     const removeIngredientHandler = id => {
