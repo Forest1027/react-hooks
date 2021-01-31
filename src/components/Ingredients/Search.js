169e4448ec6b8860b35a2007f49a992c10fea1f6
Forest1027 <azureforestryan@gmail.com>
2021-01-31 11:08:34 +0000

    clear timer

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,6 +6,7 @@ import './Search.css';
 const Search = React.memo(props => {
     const {onLoadIngredients} = props;
     const [enteredFilter, setEnteredFilter] = useState('');
+    const inputRef = useRef();
 
     // useEffect acts like componentDidUpdate.
     // It runs the function after every component update (re-render).
@@ -13,28 +14,36 @@ const Search = React.memo(props => {
     // with [] as second argument, useEffect acts like componentDidMount.
     // It runs only once after the first render
     useEffect(() => {
-        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch('https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients.json' + query).then(response => {
-            return response.json();
-        }).then(responseData => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-                loadedIngredients.push({
-                    id: key,
-                    title: responseData[key].title,
-                    amount: responseData[key].amount
+        const timer = setTimeout(() => {
+            // js closure. enteredFilter here contains the old value the it just stepped into setTimeout function
+            if(enteredFilter === inputRef.current.value) {
+                const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+                fetch('https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients.json' + query).then(response => {
+                    return response.json();
+                }).then(responseData => {
+                    const loadedIngredients = [];
+                    for (const key in responseData) {
+                        loadedIngredients.push({
+                            id: key,
+                            title: responseData[key].title,
+                            amount: responseData[key].amount
+                        });
+                    }
+                    onLoadIngredients(loadedIngredients);
                 });
             }
-            onLoadIngredients(loadedIngredients);
-        });
-    }, [enteredFilter, onLoadIngredients]);
+        }, 500);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [enteredFilter, onLoadIngredients, inputRef]);
 
     return (
         <section className="search">
             <Card>
                 <div className="search-input">
                     <label>Filter by Title</label>
-                    <input type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)}/>
+                    <input ref={inputRef} type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)}/>
                 </div>
             </Card>
         </section>
