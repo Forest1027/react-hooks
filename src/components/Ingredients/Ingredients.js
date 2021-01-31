8d2ddb3a51e8fc23c5861a1744e195b6ebcc03ad
Forest1027 <azureforestryan@gmail.com>
2021-01-31 10:52:06 +0000

    search function, useCallback

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useCallback} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
@@ -7,26 +7,12 @@ import Search from './Search';
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
 
-    // useEffect acts like componentDidUpdate.
-    // It runs the function after every component update (re-render).
-    // But you can change how often it gets re-run using the second input.
-    // with [] as second argument, useEffect acts like componentDidMount.
-    // It runs only once after the first render
-    useEffect(() => {
-        fetch('https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients.json').then(response => {
-            return response.json();
-        }).then(responseData => {
-            const loadedIngredients = [];
-            for (const key in responseData) {
-                loadedIngredients.push({
-                    id: key,
-                    title: responseData[key].title,
-                    amount: responseData[key].amount
-                });
-            }
-            setUserIngredients(loadedIngredients);
-        });
-    }, []);
+    // useCallback: return a memoized version of the callback function that only changes if one of the dependencies has changed,
+    // or else it will return a cached version
+    // it is helpful to prevent unnecessary renders.
+    const filteredIngredientsHandler = useCallback(filteredIngredients => {
+        setUserIngredients(filteredIngredients);
+    }, [setUserIngredients]);
 
     const addIngredientHandler = ingredient => {
         fetch('https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients.json', {
@@ -49,7 +35,7 @@ const Ingredients = () => {
             <IngredientForm onAddIngredient={addIngredientHandler}/>
 
             <section>
-                <Search/>
+                <Search onLoadIngredients={filteredIngredientsHandler}/>
                 <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
             </section>
         </div>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
-  return (
-    <section className="search">
-      <Card>
-        <div className="search-input">
-          <label>Filter by Title</label>
-          <input type="text" />
-        </div>
-      </Card>
-    </section>
-  );
+    const {onLoadIngredients} = props;
+    const [enteredFilter, setEnteredFilter] = useState('');
+
+    // useEffect acts like componentDidUpdate.
+    // It runs the function after every component update (re-render).
+    // But you can change how often it gets re-run using the second input.
+    // with [] as second argument, useEffect acts like componentDidMount.
+    // It runs only once after the first render
+    useEffect(() => {
+        const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch('https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients.json' + query).then(response => {
+            return response.json();
+        }).then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+                loadedIngredients.push({
+                    id: key,
+                    title: responseData[key].title,
+                    amount: responseData[key].amount
+                });
+            }
+            onLoadIngredients(loadedIngredients);
+        });
+    }, [enteredFilter, onLoadIngredients]);
+
+    return (
+        <section className="search">
+            <Card>
+                <div className="search-input">
+                    <label>Filter by Title</label>
+                    <input type="text" value={enteredFilter} onChange={event => setEnteredFilter(event.target.value)}/>
+                </div>
+            </Card>
+        </section>
+    );
 });
 
 export default Search;
