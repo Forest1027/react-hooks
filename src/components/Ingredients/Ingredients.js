006e211700e10bd7a2a49058a1ea62345573d6f3
Forest1027 <azureforestryan@gmail.com>
2021-02-01 07:48:28 +0000

    loading

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -6,6 +6,7 @@ import Search from './Search';
 
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     // useCallback: return a memoized version of the callback function that only changes if one of the dependencies has changed,
     // or else it will return a cached version
@@ -15,11 +16,13 @@ const Ingredients = () => {
     }, [setUserIngredients]);
 
     const addIngredientHandler = ingredient => {
+        setIsLoading(true);
         fetch('https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients.json', {
             method: 'POST',
             body: JSON.stringify(ingredient),
             headers: {'Content-Type': 'application/json'}
         }).then(response => {
+            setIsLoading(false);
             return response.json();
         }).then(responseData => {
             setUserIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}]);
@@ -27,16 +30,18 @@ const Ingredients = () => {
     };
 
     const removeIngredientHandler = id => {
+        setIsLoading(true);
         fetch(`https://react-hooks-update-948aa-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
             method: 'DELETE'
         }).then(response => {
+            setIsLoading(false);
             setUserIngredients(prevIngredients => prevIngredients.filter(ig => ig.id !== id))
         });
     };
 
     return (
         <div className="App">
-            <IngredientForm onAddIngredient={addIngredientHandler}/>
+            <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
             <section>
                 <Search onLoadIngredients={filteredIngredientsHandler}/>
